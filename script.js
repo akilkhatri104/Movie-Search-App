723345689ac71560f111b29cfda7946412974cf3
Akil Khatri <84614450+akilkhatri104@users.noreply.github.com>
2024-08-21 18:49:01 +0530

    Basic Details of Movies

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const apiKey = '0bcea48136fbfeb6ae2a0441736f5ff7'
 const form = document.querySelector('form')
 const titleInput = document.querySelector('#titleInput')
 const queryResultsDiv = document.querySelector('#queryResults')
+const movieDetailsSection = document.querySelector('#movieDetails')
 
 form.addEventListener('submit',async e => {
     try {
@@ -17,8 +18,87 @@ form.addEventListener('submit',async e => {
 
 })
 
+function displayQueryResults(data){
+    const results = data.results
+
+    queryResultsDiv.innerHTML = ''
+    queryResultsDiv.style.display = 'flex'
+    movieDetailsSection.style.display = 'none'
 
 
+    results.forEach( result => {
+        // console.log(result);
+
+        const newDiv = document.createElement('div')
+        newDiv.id = 'searchResult'
+        queryResultsDiv.appendChild(newDiv)
+
+        const posterTag = document.createElement('img')
+        posterTag.src = `https://image.tmdb.org/t/p/w154${result.poster_path}`
+        newDiv.appendChild(posterTag)
+
+
+        const movieTitle = document.createElement('p')
+        movieTitle.innerHTML = result.title
+        newDiv.appendChild(movieTitle)
+
+        const releaseDate = document.createElement('p')
+        releaseDate.innerHTML = result.release_date
+        newDiv.appendChild(releaseDate)
+
+        const moreInfo = document.createElement('button')
+        moreInfo.innerHTML = 'More Info'
+        newDiv.appendChild(moreInfo)
+
+        moreInfo.addEventListener('click',async e => {
+            e.preventDefault()
+
+            const data = await getMovieDetails(result.id)
+            displayMovieDetail(data)
+        })
+
+        
+        
+    } )
+}
+
+function displayMovieDetail(data){
+    queryResultsDiv.style.display = 'none'
+    movieDetailsSection.style.display = 'block'
+    movieDetailsSection.innerHTML = ''
+
+    // const leftSection = document.querySelector('#leftSection')
+    // const rightSection = document.querySelector('#rightSection')
+
+    // leftSection.innerHTML = ''
+    // rightSection.innerHTML = ''
+
+    const posterTag = document.createElement('img')
+    posterTag.src = `https://image.tmdb.org/t/p/w342${data.poster_path}`
+    movieDetailsSection.appendChild(posterTag)
+
+    const tagline = document.createElement('p')
+    tagline.innerHTML = `<i>${data.tagline}</i>`
+    tagline.style.textAlign = 'center'
+    movieDetailsSection.appendChild(tagline)
+
+    const title = document.createElement('p')
+    title.innerHTML = `<strong>Title</strong>: ${data.original_title}`
+    movieDetailsSection.appendChild(title)
+
+    const overview = document.createElement('p')
+    overview.innerHTML = `<strong>Overview</strong>: ${data.overview}`
+    movieDetailsSection.appendChild(overview)
+
+    const runtime = document.createElement('p')
+    runtime.innerHTML = `<strong>Runtime</strong>: ${data.runtime} mins`
+    movieDetailsSection.appendChild(runtime)
+
+    const release_date = document.createElement('p')
+    release_date.innerHTML = `<strong>Release Date: ${data.release_date}`
+    movieDetailsSection.appendChild(release_date)
+
+}
 
 async function searchMovie(title = ""){
     try {
@@ -40,37 +120,20 @@ console.log(apiCall);
     }
 }
 
-function displayQueryResults(data){
-    const results = data.results
-
-    queryResultsDiv.innerHTML = ''
-
-    results.forEach( result => {
-        console.log(result);
-
-        const newDiv = document.createElement('div')
-        newDiv.id = 'searchResult'
-        queryResultsDiv.appendChild(newDiv)
-
-        const posterTag = document.createElement('img')
-        posterTag.src = `https://image.tmdb.org/t/p/w154${result.poster_path}`
-
-        const movieTitle = document.createElement('p')
-        movieTitle.innerHTML = result.title
-
-        const releaseDate = document.createElement('p')
-        releaseDate.innerHTML = result.release_date
-
-        const moreDetails = document.createElement('button')
-        moreDetails.innerHTML = 'More Details'
-
+async function getMovieDetails(id){
+    try {
+        const apiCall = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`
+        console.log(apiCall);
         
-        newDiv.appendChild(posterTag)
-        newDiv.appendChild(movieTitle)
-        newDiv.appendChild(releaseDate)
-        newDiv.appendChild(moreDetails)
 
-        
-        
-    } )
+        const response = await fetch(apiCall)
+        if (!response.ok) {
+            throw new Error('Error fetching movie details')
+        }
+
+        const data = await response.json()
+        return data
+    } catch (error) {
+        alert(error)
+    }
 }
